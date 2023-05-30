0079c246864cb6dd7af8137007ee5292627628de
HSUWEI0818 <wei930818@gmail.com>
2023-05-30 23:32:41 +0800

    main註解

--- main.py
+++ main.py
@@ -1,18 +1,22 @@
 '''
 檔案名稱:main.py
 負責人:B11109046 徐名崴
-用途:執行稅費繳納系統的各種功能
+用途:執行繳費系統的各種功能
 '''
 
 import incometax
 import utility
 import vehicle
 
+#bonusList儲存回饋金額
+#nameList儲存繳費名稱
 bonusList = []*4
 nameList = []*4
 
+#繳費系統運作
 while (True):
     
+        #選擇繳費功能
         print("歡迎使用繳費系統！請選擇您需要的功能：")
         print("(1) 繳納綜合所得稅相關功能")
         print("(2) 繳納水電費相關功能")
@@ -22,6 +26,7 @@ while (True):
         
         choice = input("請輸入功能編號：")
 
+        #對應所選繳費功能進行運算並將結果儲存至陣列
         match choice:
             case "1":
                 total_income_tax = float(input("請輸入所得稅淨額："))
