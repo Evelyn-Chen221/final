8b711196a1c785ad88b4be96f05d94fe5732605b
Evelyn-Chen221 <qaz930221@gmail.com>
2023-05-30 13:20:10 +0800

    incometax&utility showcard

---
+++ incometax.py
@@ -0,0 +1,27 @@
+'''
+檔案名稱:incometax.py
+負責人:B11109049 陳宜妏
+用途:繳納綜合所得稅相關功能(包含根據所得、顯示信用卡、計算回饋)
+'''
+#total_income_tax為所得稅淨額
+#tax_money為總稅額
+def calculate(total_income_tax):
+    if total_income_tax <= 560000:
+        tax_money = total_income_tax * 0.05
+    elif total_income_tax >= 560001 and total_income_tax <= 1260000:
+        tax_money = total_income_tax  * 0.12 - 39200
+    elif total_income_tax >= 1260001 and total_income_tax <= 2520000:
+        tax_money = total_income_tax* 0.2 - 140000
+    elif total_income_tax >= 2520001 and total_income_tax <= 4720000:
+        tax_money = total_income_tax* 0.3 - 392000
+    else:
+        tax_money = total_income_tax * 0.4 - 86400
+    return tax_money
+#showTaxCard顯示可使用的信用卡
+def showTaxcard():
+    print("     信用卡名稱　　　　回饋比例　　　回饋金上限")
+    print("    中信Line pay　　　　0.2%　　　　    無")
+    print("   匯豐銀行匯鑽卡　　　　 1%　　　　    500")
+    print("台新@GoGoCash御璽卡     0.2%　　　　   2000")
+
+
---
+++ utility.py
@@ -0,0 +1,12 @@
+'''
+檔案名稱:utility.py
+負責人:B11109049 陳宜妏
+用途:繳納水電費相關功能(包含顯示信用卡、計算回饋)
+'''
+
+
+#showUtilityCard顯示可使用的信用卡
+def showUtilitycard():
+    print("    信用卡名稱        回饋比例     回饋上限")
+    print("  國泰世華CUBE卡         1%          200")
+    print("台新@GoGoCash御璽卡     3.8%         1000")
